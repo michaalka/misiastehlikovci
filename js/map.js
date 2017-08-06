9d386f874abdd4df82b0f9e39bb1f5870c1b5470
michaalka <m.mokcsayova@gmail.com>
2017-08-06 19:39:42 +0200

    adjusted map coordinates

--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 function initMap() {
     var myOptions = {
         zoom: 11,
-        center: new google.maps.LatLng(48.767438799279056, 21.268564683593688),
+        center: new google.maps.LatLng(48.785676, 21.128146),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scrollwheel:  false
     };
     map = new google.maps.Map(document.getElementById('map'), myOptions);
     marker = new google.maps.Marker({
         map: map,
-        position: new google.maps.LatLng(48.767438799279056, 21.268564683593688)
+        position: new google.maps.LatLng(48.785676, 21.128146)
     });
     infowindow = new google.maps.InfoWindow({content:
         '<strong>Hotel Dam</strong><br>Hotel Dam, Košická Belá 220<br>044 65  Košická Belá<br>'
